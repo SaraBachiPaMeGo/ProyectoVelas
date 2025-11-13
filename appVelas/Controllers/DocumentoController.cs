bd893ba1d2720d9ee3acb2293cc795c138cb76c5
Sara Bachiller <sarabachiller@gmail.com>
2025-11-13 16:55:51 +0100

    Cambiadas las url del front para actualizar y detalles. Creado el controller pero no hay métodos

---
+++ appVelas/Controllers/DocumentoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using appVelas.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace appVelas.Controllers
+{
+    public class DocumentoController : Controller
+    {
+        private readonly RepositoryVelas _velaRepo;
+        private readonly RepositoryMoldes _moldeRepo;
+        private readonly RepositoryFragancias _fragRepo;
+        private readonly RepositoryPigmentos _pigRepo;
+        private readonly RepositoryCeras _ceraRepo;
+        private readonly RepositoryMechas _mechaRepo;
+        private readonly RepositoryVelaFragancias _vFragRepo;
+        private readonly RepositoryVelaPigmentos _vPigRepo;
+        private readonly RepositoryPedidos _pediRepo;
+        private readonly RepositoryClientes _cliRepo;
+
+        public DocumentoController(RepositoryVelas velaRepo, RepositoryMoldes moldeRepo, RepositoryFragancias fragRepo,
+            RepositoryPigmentos pigRepo, RepositoryCeras ceraRepo, RepositoryMechas mechaRepo, RepositoryVelaFragancias velaFragRepo,
+            RepositoryVelaPigmentos velaPigRepo, RepositoryPedidos pediRepo, RepositoryClientes cliRepo)
+        {
+            _velaRepo = velaRepo;
+            _moldeRepo = moldeRepo;
+            _fragRepo = fragRepo;
+            _pigRepo = pigRepo;
+            _ceraRepo = ceraRepo;
+            _mechaRepo = mechaRepo;
+            _vFragRepo = velaFragRepo;
+            _vPigRepo = velaPigRepo;
+            _pediRepo = pediRepo;
+            _cliRepo = cliRepo;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
